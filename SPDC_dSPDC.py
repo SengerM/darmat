Fixed dark cutoff angle calculation

SPDC_dSPDC.py
@@ -20,7 +20,7 @@ def theta_cutoff_SPDC(n_signal, n_idler, alpha):
 
 def theta_cutoff_dSPDC(lambda_pump, l, n_pump, n_signal, alpha, m):
 	omega_pump = 2*np.pi*const.c/lambda_pump
-	return np.arcsin(((1-alpha)**2 - m**2*const.c**2/omega_pump**2/const.hbar**2)**.5/alpha/n_signal)
+	return np.arcsin(((1-alpha)**2 - m**2*const.c**4/omega_pump**2/const.hbar**2)**.5/alpha/n_signal)
 
 def theta_phasematch_dSPDC(lambda_pump, l, n_pump, n_signal, alpha, m, omega_pump):
 	costheta = (n_pump**2 + alpha**2*n_signal**2 - (1-alpha)**2 - m**2*const.c*4/omega_pump**2/const.hbar**2)/2/alpha/n_pump/n_signal
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 	N_SIGNAL = 3
 	N_IDLER = N_SIGNAL
 	ALPHA = .5
-	DARK_PHOTON_MASS = 0#1.1e-36
+	DARK_PHOTON_MASS = 1.1e-36
 	
 	theta = np.linspace(0, np.pi/2, 99999)
 
@@ -54,49 +54,63 @@ if __name__ == '__main__':
 	
 	fig, ax = plt.subplots()
 	theta_q_last = float('nan')
-	for q in range(-300,300):
-		if q == 0:
-			continue
-		xq = np.sqrt(
-			   1 - ((LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2 + N_SIGNAL**2*ALPHA**2 - 
-			   N_IDLER**2*(1-ALPHA)**2)**2 /
-			   (4*N_SIGNAL**2*ALPHA**2*(LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2)
-			  )
-		theta_q = np.arcsin(xq)
-		if theta_q < theta_q_last:
-			break
-		theta_q_last = theta_q
-		ax.plot(
-				theta_q*180/np.pi*np.array([1,1]),
-				[0, 1],
-				color = (.7,.7,1)
-			   )
+	# ~ for q in range(-300,300):
+		# ~ if q == 0:
+			# ~ continue
+		# ~ xq = np.sqrt(
+			   # ~ 1 - ((LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2 + N_SIGNAL**2*ALPHA**2 - 
+			   # ~ N_IDLER**2*(1-ALPHA)**2)**2 /
+			   # ~ (4*N_SIGNAL**2*ALPHA**2*(LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2)
+			  # ~ )
+		# ~ theta_q = np.arcsin(xq)
+		# ~ if theta_q < theta_q_last:
+			# ~ break
+		# ~ theta_q_last = theta_q
+		# ~ ax.plot(
+				# ~ theta_q*180/np.pi*np.array([1,1]),
+				# ~ [0, 1],
+				# ~ color = (.7,.7,1)
+			   # ~ )
 	
-	theta_q_last = float('nan')
-	for q in range(-200,100):
-		xq = np.sqrt(
-			   1 - ((LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2 + N_SIGNAL**2*ALPHA**2 - 
-			   (1-ALPHA)**2 + DARK_PHOTON_MASS**2*const.c**4/omega_pump**2/const.hbar**2)**2 /
-			   (4*N_SIGNAL**2*ALPHA**2*(LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2)
-			  )
-		theta_q = np.arcsin(xq)
-		if theta_q < theta_q_last:
-			break
-		theta_q_last = theta_q
-		ax.plot(
-				theta_q*180/np.pi*np.array([1,1]),
-				[0, 1],
-				color = (1,.7,.7)
-			   )
+	# ~ theta_q_last = float('nan')
+	# ~ for q in range(-200,100):
+		# ~ xq = np.sqrt(
+			   # ~ 1 - ((LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2 + N_SIGNAL**2*ALPHA**2 - 
+			   # ~ (1-ALPHA)**2 + DARK_PHOTON_MASS**2*const.c**4/omega_pump**2/const.hbar**2)**2 /
+			   # ~ (4*N_SIGNAL**2*ALPHA**2*(LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2)
+			  # ~ )
+		# ~ theta_q = np.arcsin(xq)
+		# ~ if theta_q < theta_q_last:
+			# ~ break
+		# ~ theta_q_last = theta_q
+		# ~ ax.plot(
+				# ~ theta_q*180/np.pi*np.array([1,1]),
+				# ~ [0, 1],
+				# ~ color = (1,.7,.7)
+			   # ~ )
 	
 	ax.plot(
 			theta_phasematch_SPDC(LAMBDA_PUMP, L, N_PUMP, N_SIGNAL, N_IDLER, ALPHA)*180/np.pi*np.array([1,1]),
 			[0, 1],
-			color = (0,0,0)
+			color = (0,.8,0),
+			label = 'Phase matching angle'
 		   )
 	ax.plot(
 			theta_phasematch_dSPDC(LAMBDA_PUMP, L, N_PUMP, N_SIGNAL, ALPHA, DARK_PHOTON_MASS, omega_pump)*180/np.pi*np.array([1,1]),
 			[0, 1],
+			color = (0,.8,0)
+		   )
+	ax.plot(
+			[180/np.pi*theta_cutoff_SPDC(N_SIGNAL, N_IDLER, ALPHA)]*2,
+			[0,1],
+			linestyle = '--',
+			color = (0,0,0),
+			label = 'Cutoff angle'
+		   )
+	ax.plot(
+			[180/np.pi*theta_cutoff_dSPDC(LAMBDA_PUMP, L, N_PUMP, N_SIGNAL, ALPHA, DARK_PHOTON_MASS)]*2,
+			[0,1],
+			linestyle = '--',
 			color = (0,0,0)
 		   )
 	