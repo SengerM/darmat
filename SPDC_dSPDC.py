6203c72f7e9a7873c6b2a86f4b9901206f610c13
Matias Senger <m.senger@hotmail.com>
2019-10-10 01:23:55 -0500

    Functions for calculating the zeros of SPDC and dark SPDC

--- SPDC_dSPDC.py
+++ SPDC_dSPDC.py
@@ -33,6 +33,53 @@ def dSPDC_intensity_profile(theta, lambda_pump, l, n_pump, n_signal, alpha, m):
 				   - ((1-alpha)**2 - m**2*const.c**4/const.hbar**2/omega_pump**2
 				   - alpha**2*n_signal**2*np.sin(theta)**2)**.5))**2
 
+def SPDC_zeros(lambda_pump, l, n_pump, n_signal, n_idler, alpha, q_try = range(-300,300)):
+	theta_zeros = []
+	q_zeros = []
+	for q in q_try:
+		if q == 0:
+			continue
+		xq = np.sqrt(
+			   1 - ((lambda_pump/l*q/np.pi - n_pump)**2 + n_signal**2*alpha**2 - 
+			   n_idler**2*(1-alpha)**2)**2 /
+			   (4*n_signal**2*alpha**2*(lambda_pump/l*q/np.pi - n_pump)**2)
+			  )
+		if xq < -1 or xq > 1 or np.isnan(xq):
+			continue
+		theta_zeros.append(np.arcsin(xq))
+		q_zeros.append(q)
+		if len(theta_zeros) == 1:
+			continue
+		if theta_zeros[-1] < theta_zeros[-2]: # This means that we are no longer finding zeros
+			theta_zeros = theta_zeros[:-2]
+			q_zeros = q_zeros[:-2]
+			break
+	return q_zeros, theta_zeros
+
+def dSPDC_zeros(lambda_pump, l, n_pump, n_signal, alpha, m, q_try = range(-300,300)):
+	omega_pump = 2*np.pi*const.c/lambda_pump
+	theta_zeros = []
+	q_zeros = []
+	for q in q_try:
+		if q == 0:
+			continue
+		xq = np.sqrt(
+			   1 - ((lambda_pump/l*q/np.pi - n_pump)**2 + n_signal**2*alpha**2 - 
+			   (1-alpha)**2 + m**2*const.c**4/omega_pump**2/const.hbar**2)**2 /
+			   (4*n_signal**2*alpha**2*(lambda_pump/l*q/np.pi - n_pump)**2)
+			  )
+		if xq < -1 or xq > 1 or np.isnan(xq):
+			continue
+		theta_zeros.append(np.arcsin(xq))
+		q_zeros.append(q)
+		if len(theta_zeros) == 1:
+			continue
+		if theta_zeros[-1] < theta_zeros[-2]: # This means that we are no longer finding zeros
+			theta_zeros = theta_zeros[:-2]
+			q_zeros = q_zeros[:-2]
+			break
+	return q_zeros, theta_zeros
+
 if __name__ == '__main__':
 	import matplotlib.pyplot as plt
 	
@@ -40,10 +87,10 @@ if __name__ == '__main__':
 	L = .007e-3
 	D = 10e-2
 	N_PUMP = 1.5
-	N_SIGNAL = 3
+	N_SIGNAL = 2.5
 	N_IDLER = N_SIGNAL
 	ALPHA = .5
-	DARK_PHOTON_MASS = 5e-37
+	DARK_PHOTON_MASS = .8e-37
 	
 	theta = np.linspace(0, np.pi/2, 99999)
 
@@ -54,41 +101,16 @@ if __name__ == '__main__':
 	print('theta phasematch dSPDC = ' + str(theta_phasematch_dSPDC(LAMBDA_PUMP, N_PUMP, N_SIGNAL, ALPHA, DARK_PHOTON_MASS)*180/np.pi))
 	
 	fig, ax = plt.subplots()
-	theta_q_last = float('nan')
-	# ~ for q in range(-300,300):
-		# ~ if q == 0:
-			# ~ continue
-		# ~ xq = np.sqrt(
-			   # ~ 1 - ((LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2 + N_SIGNAL**2*ALPHA**2 - 
-			   # ~ N_IDLER**2*(1-ALPHA)**2)**2 /
-			   # ~ (4*N_SIGNAL**2*ALPHA**2*(LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2)
-			  # ~ )
-		# ~ theta_q = np.arcsin(xq)
-		# ~ if theta_q < theta_q_last:
-			# ~ break
-		# ~ theta_q_last = theta_q
-		# ~ ax.plot(
-				# ~ theta_q*180/np.pi*np.array([1,1]),
-				# ~ [0, 1],
-				# ~ color = (.7,.7,1)
-			   # ~ )
 	
-	# ~ theta_q_last = float('nan')
-	# ~ for q in range(-200,100):
-		# ~ xq = np.sqrt(
-			   # ~ 1 - ((LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2 + N_SIGNAL**2*ALPHA**2 - 
-			   # ~ (1-ALPHA)**2 + DARK_PHOTON_MASS**2*const.c**4/omega_pump**2/const.hbar**2)**2 /
-			   # ~ (4*N_SIGNAL**2*ALPHA**2*(LAMBDA_PUMP/L*q/np.pi - N_PUMP)**2)
-			  # ~ )
-		# ~ theta_q = np.arcsin(xq)
-		# ~ if theta_q < theta_q_last:
-			# ~ break
-		# ~ theta_q_last = theta_q
-		# ~ ax.plot(
-				# ~ theta_q*180/np.pi*np.array([1,1]),
-				# ~ [0, 1],
-				# ~ color = (1,.7,.7)
-			   # ~ )
+	q_zeros, theta_zeros = SPDC_zeros(LAMBDA_PUMP, L, N_PUMP, N_SIGNAL, N_IDLER, ALPHA)
+	for k,t in enumerate(theta_zeros):
+		ax.plot([t*180/np.pi]*2, [0,1], color = (.8,.8,1))
+		ax.text(t*180/np.pi, 1, str(q_zeros[k]), color = (.8,.8,1))
+	
+	q_zeros, theta_zeros = dSPDC_zeros(LAMBDA_PUMP, L, N_PUMP, N_SIGNAL, ALPHA, DARK_PHOTON_MASS)
+	for k,t in enumerate(theta_zeros):
+		ax.plot([t*180/np.pi]*2, [0,1], color = (1,.8,.8))
+		ax.text(t*180/np.pi, 1, str(q_zeros[k]), color = (1,.8,.8))
 	
 	ax.plot(
 			theta_phasematch_SPDC(LAMBDA_PUMP, L, N_PUMP, N_SIGNAL, N_IDLER, ALPHA)*180/np.pi*np.array([1,1]),
