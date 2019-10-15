0fb13d14aafb404f150ef81b40bf3d1f7a107e74
Matias Senger <m.senger@hotmail.com>
2019-10-15 12:19:54 -0500

    Improved calculation

--- darmat/SPDC.py
+++ darmat/SPDC.py
@@ -17,7 +17,8 @@ def theta_phasematch_SPDC(lambda_pump, l, n_pump, n_signal, n_idler, alpha):
 	return np.arccos(costheta) if costheta >= -1 and costheta <= 1 else float('nan')
 
 def theta_cutoff_SPDC(n_signal, n_idler, alpha):
-	return np.arcsin((1-alpha)/alpha*n_idler/n_signal)
+	arg = (1-alpha)/alpha*n_idler/n_signal
+	return np.arcsin(arg) if arg <= 1 else float('nan') 
 
 def SPDC_zeros(lambda_pump, l, n_pump, n_signal, n_idler, alpha, q_try = range(-300,300)):
 	theta_zeros = []
