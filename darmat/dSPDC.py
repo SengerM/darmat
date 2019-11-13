Fixed another calculation problem, ja

darmat/dSPDC.py
@@ -26,7 +26,8 @@ class dSPDC:
 		self.omega_pump = 2*np.pi*const.c/lambda_pump
 		
 		radicando = (1-alpha)**2 - m_dark_photon**2*const.c**4/self.omega_pump**2/const.hbar**2
-		self.Xi = ((1-alpha)**2 - m_dark_photon**2*const.c**4/self.omega_pump**2*const.hbar**2)**.5 if radicando >= 0 else float('nan')
+		self.Xi = radicando**.5 if radicando >= 0 else float('nan')
+		print('Xi = ' + str(self.Xi))
 		self.a = alpha*n_signal/self.Xi
 		
 		self.independent_theta_name = theta_name(self.a).get('independent')