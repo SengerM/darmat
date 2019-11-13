Fixed calculation problem

darmat/dSPDC.py
@@ -25,7 +25,7 @@ class dSPDC:
 		
 		self.omega_pump = 2*np.pi*const.c/lambda_pump
 		
-		radicando = (1-alpha)**2 - m_dark_photon**2*const.c**4/self.omega_pump**2*const.hbar**2
+		radicando = (1-alpha)**2 - m_dark_photon**2*const.c**4/self.omega_pump**2/const.hbar**2
 		self.Xi = ((1-alpha)**2 - m_dark_photon**2*const.c**4/self.omega_pump**2*const.hbar**2)**.5 if radicando >= 0 else float('nan')
 		self.a = alpha*n_signal/self.Xi
 		