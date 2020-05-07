a9833ad40a492c16ace451ef8665f69e87a5843f
Matias Senger <m.senger@hotmail.com>
2020-05-07 01:03:04 -0300

    Implemented __str__ methods and get lambda

--- darmat/emission_events.py
+++ darmat/emission_events.py
@@ -1,11 +1,17 @@
 import numpy as np
+import scipy.constants as const
 
 class PhotonEmissionEvent:
 	def __init__(self, theta, phi, omega):
 		self.vars = {'theta': theta, 'phi': phi, 'omega': omega}
 	
 	def get(self, what):
+		if what == 'lambda':
+			return 2*const.pi*const.c/self.get('omega')
 		return self.vars.get(what)
+		
+	def __str__(self):
+		return 'PhotonEmissionEvent: theta=' + str(self.get('theta')*180/np.pi) + ' deg, phi=' + str(self.get('phi')*180/np.pi) + ' deg, lambda=' + str(const.c/self.get('omega')*2*const.pi*1e9) + ' nm'
 
 class SPDCEvent:
 	def __init__(self, theta_s, omega_s, theta_i, phi_s=None, phi_i=None, omega_i=None, omega_p=None):
@@ -37,3 +43,6 @@ class SPDCEvent:
 			return ws/(ws+wi)
 		else:
 			return None
+	
+	def __str__(self):
+		return 'SPDCEvent:\nSignal: ' + str(self.signal) + '\nIdler: ' + str(self.idler)
