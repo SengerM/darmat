fc0e14cc02ef9d4222f47b2337721dcba5184a2f
Matias Senger <m.senger@hotmail.com>
2020-05-07 11:05:29 -0300

    Added the "OldSPDC" class back. Removed old stuff from the new "SPDC" class. Improved the argument handling in "single photon intensity"

--- darmat/SPDC.py
+++ darmat/SPDC.py
@@ -5,6 +5,95 @@ from .common_functions import W_in_branch_as_function_of_independent_theta, W_in
 from .crystal import Crystal
 import numbers
 
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as const
+from .common_functions import *
+
+class OldSPDC:
+	def __init__(self, lambda_pump, crystal_l, n_pump, n_signal, n_idler, alpha):
+		if n_pump < 0 or n_signal < 0 or n_idler < 0:
+			raise ValueError('Negative refractive index received! I do not support this...')
+		if alpha < 0 or alpha > 1:
+			raise ValueError('The value of "alpha" must be between 0 and 1 (by definition of alpha).')
+		if crystal_l < 0:
+			raise ValueError('The value of "crystal_l" must be positive.')
+		if lambda_pump < 0:
+			raise ValueError('The value of "lambda_pump" must be positive.')
+		
+		self.lambda_pump = lambda_pump # In meters.
+		self.crystal_l = crystal_l # Nonlinear medium length in meters.
+		self.n_pump = n_pump
+		self.n_signal = n_signal
+		self.n_idler = n_idler
+		self.alpha = alpha # Ratio of signal frequency to pump frequency, i.e. omega_signal/omega_pump.
+		
+		self.omega_pump = 2*np.pi*const.c/lambda_pump
+		
+		self.Xi = n_idler*(1-alpha)
+		self.a = alpha*n_signal/self.Xi
+		
+		self.independent_theta_name = theta_name(self.a).get('independent')
+		self.dependent_theta_name = theta_name(self.a).get('dependent')
+		self.q_zeros, self.independent_theta_zeros, _ = zeros_of_W_in_branch_1(self.lambda_pump, self.crystal_l, self.n_pump, self.n_signal, self.alpha, self.Xi)
+	
+	def W_as_function_of_independent_theta(self, independent_theta_vals = None, branch = 'branch_1'):
+		if independent_theta_vals is None:
+			minimum_distance_between_zeros = min(np.diff(self.independent_theta_zeros))
+			theta_step = minimum_distance_between_zeros/20
+			independent_theta_vals = np.linspace(0, np.pi, int(np.pi/theta_step))
+		W, independent_theta_name = W_in_branch_as_function_of_independent_theta(
+											self.lambda_pump, 
+											self.crystal_l, 
+											self.n_pump, 
+											self.n_signal, 
+											self.alpha, 
+											self.Xi, 
+											independent_theta_vals, 
+											branch)
+		return independent_theta_vals, W
+	
+	def W_as_function_of_dependent_theta(self, dependent_theta_vals = None, branch = 'branch_1'):
+		if dependent_theta_vals is None:
+			minimum_distance_between_zeros = min(np.diff(self.independent_theta_zeros))
+			theta_step = minimum_distance_between_zeros/20
+			dependent_theta_vals = np.linspace(0, np.pi, int(np.pi/theta_step))
+		W, dependent_theta_name = W_in_branch_as_function_of_dependent_theta(
+											self.lambda_pump, 
+											self.crystal_l, 
+											self.n_pump, 
+											self.n_signal, 
+											self.alpha, 
+											self.Xi, 
+											dependent_theta_vals, 
+											branch)
+		W = np.array(W)
+		W[np.isnan(W)] = 0
+		return dependent_theta_vals, W
+	
+	def observed_W(self, theta = None):
+		# The parameter "theta" is the detector angle.
+		theta, W1_indep = self.W_as_function_of_independent_theta(independent_theta_vals = theta, branch = 'branch_1')
+		theta, W2_indep = self.W_as_function_of_independent_theta(independent_theta_vals = theta, branch = 'branch_2')
+		theta, W1_dep = self.W_as_function_of_dependent_theta(dependent_theta_vals = theta, branch = 'branch_1')
+		theta, W2_dep = self.W_as_function_of_dependent_theta(dependent_theta_vals = theta, branch = 'branch_2')
+		total_W = W1_indep + W2_indep + W1_dep + W2_dep
+		return theta, total_W, W1_indep, W2_indep, W1_dep, W2_dep
+	
+	def f_factor(self, theta = None):
+		theta_vals, f_SPDC, _, _, _, _ = self.observed_W(theta)
+		if theta is None:
+			return theta_vals, f_SPDC
+		else:
+			return f_SPDC
+	
+	def plot_W_in_thetas_space(self, theta_s=None, theta_i=None):
+		if theta_s is None:
+			theta_s = np.linspace(0,180/180*np.pi,999)
+		if theta_i is None:
+			theta_i = np.linspace(0,180/180*np.pi,999)
+		return plot_W_in_thetas_space(self.lambda_pump, self.crystal_l, self.n_pump, self.n_signal, self.alpha, self.Xi, theta_s, theta_i)
+
 class SPDC:
 	def __init__(self, lambda_pump, crystal, theta_s_dipole = None, phi_s_dipole = None, theta_i_dipole = None, phi_i_dipole = None):
 		if lambda_pump < 0:
@@ -20,63 +109,6 @@ class SPDC:
 		self.theta_i_dipole = theta_i_dipole, 
 		self.phi_i_dipole = phi_i_dipole
 		
-	def W_as_function_of_independent_theta(self, independent_theta_vals, branch, alpha):
-		W, independent_theta_name = W_in_branch_as_function_of_independent_theta(
-			lambda_pump = self.lambda_pump,
-			crystal_l = self.crystal.crystal_length,
-			n_pump = self.crystal.n(wavelength = self.lambda_pump),
-			n_signal = self.crystal.n(wavelength = self.lambda_pump/alpha), 
-			alpha = alpha, 
-			Xi = Xi(n_idler = self.crystal.n(wavelength = self.lambda_pump/(1-alpha)), alpha = alpha),
-			independent_theta_vals = independent_theta_vals, 
-			branch = branch
-		)
-		return independent_theta_vals, W
-	
-	def W_as_function_of_dependent_theta(self, dependent_theta_vals, branch, alpha):
-		W, dependent_theta_name = W_in_branch_as_function_of_dependent_theta(
-			lambda_pump = self.lambda_pump,
-			crystal_l = self.crystal.crystal_length,
-			n_pump = self.crystal.n(wavelength = self.lambda_pump),
-			n_signal = self.crystal.n(wavelength = self.lambda_pump/alpha), 
-			alpha = alpha, 
-			Xi = Xi(n_idler = self.crystal.n(wavelength = self.lambda_pump/(1-alpha)), alpha = alpha),
-			dependent_theta_vals = dependent_theta_vals, 
-			branch = branch
-		)
-		W = np.array(W)
-		W[np.isnan(W)] = 0
-		return dependent_theta_vals, W
-	
-	def observed_W(self, theta, alpha):
-		# The parameter "theta" is the detector angle.
-		theta, W1_indep = self.W_as_function_of_independent_theta(independent_theta_vals = theta, branch = 'branch_1', alpha = alpha)
-		theta, W2_indep = self.W_as_function_of_independent_theta(independent_theta_vals = theta, branch = 'branch_2', alpha = alpha)
-		theta, W1_dep = self.W_as_function_of_dependent_theta(dependent_theta_vals = theta, branch = 'branch_1', alpha = alpha)
-		theta, W2_dep = self.W_as_function_of_dependent_theta(dependent_theta_vals = theta, branch = 'branch_2', alpha = alpha)
-		total_W = W1_indep + W2_indep + W1_dep + W2_dep
-		return theta, total_W, W1_indep, W2_indep, W1_dep, W2_dep
-	
-	def f_factor(self, theta, alpha):
-		theta_vals, f_SPDC, _, _, _, _ = self.observed_W(theta, alpha)
-		return f_SPDC
-	
-	def plot_W_in_thetas_space(self, alpha, theta_s=None, theta_i=None):
-		if theta_s is None:
-			theta_s = np.linspace(0,180/180*np.pi,999)
-		if theta_i is None:
-			theta_i = np.linspace(0,180/180*np.pi,999)
-		return plot_W_in_thetas_space(
-			lambda_pump = self.lambda_pump, 
-			crystal_l = self.crystal.crystal_length, 
-			n_pump = self.crystal.n(wavelength = self.lambda_pump),
-			n_signal = self.crystal.n(wavelength = self.lambda_pump/alpha), 
-			alpha = alpha, 
-			Xi = Xi(n_idler = self.crystal.n(wavelength = self.lambda_pump/(1-alpha)), alpha = alpha),
-			theta_s = theta_s, 
-			theta_i = theta_i
-		)
-	
 	def single_photon_intensity(self, theta_d = None, phi_d = None, lambda_d = None):
 		if isinstance(theta_d, numbers.Number) and isinstance(phi_d, numbers.Number) and isinstance(lambda_d, numbers.Number):
 			alpha_d = self.lambda_pump/lambda_d
@@ -115,5 +147,11 @@ class SPDC:
 				for t,p,l in zip(theta_d,phi_d,lambda_d):
 					intensities.append(self.single_photon_intensity(t,p,l))
 				return intensities
+		elif hasattr(theta_d, '__iter__') and isinstance(phi_d, numbers.Number) and isinstance(lambda_d, numbers.Number):
+			return self.single_photon_intensity(theta_d, [phi_d]*len(theta_d), [lambda_d]*len(theta_d))
+		elif isinstance(theta_d, numbers.Number) and isinstance(phi_d, numbers.Number) and hasattr(lambda_d, '__iter__'):
+			return self.single_photon_intensity([theta_d]*len(lambda_d), [phi_d]*len(lambda_d), lambda_d)
+		elif isinstance(theta_d, numbers.Number) and hasattr(phi_d, '__iter__') and isinstance(lambda_d, numbers.Number):
+			return self.single_photon_intensity([theta_d]*len(phi_d), phi_d, [lambda_d]*len(phi_d))
 		else:
 			raise NotImplementedError('The combination of parameters you gave me is not yet implemented, sorry')
